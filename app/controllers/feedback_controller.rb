9b00a2a80942014ce2e9ba42a31205ae96f50611
Nemo Djuric <nemo.djuric@gmail.com>
2016-11-16 16:17:42 -0500

    Added feature test for form submission and corrected style issues

--- app/controllers/feedback_controller.rb
+++ app/controllers/feedback_controller.rb
@@ -1,23 +1,22 @@
 class FeedbackController < ApplicationController
-
   def new
-	  if Rails.env.development?
-	    params[:app] = 'caseflow'
-		  params[:username] = 'ndjuric'
-		end
-		@feedback = Feedback.new
-	  session[:app] = params[:app]
-	  session[:username] = params[:username]
-		#puts params.inspect
+    if Rails.env.development? || Rails.env.test?
+      params[:app] = "caseflow"
+      params[:username] = "ndjuric"
+    end
+    @feedback = Feedback.new
+    session[:app] = params[:app]
+    session[:username] = params[:username]
+    # puts params.inspect
   end
 
   def create
-    #puts feedback_params
+    # puts feedback_params
     @feedback = Feedback.new(feedback_params)
     if @feedback.save
-      render 'success'
+      render "success"
     else
-      render 'new'
+      render "new"
     end
   end
 
@@ -25,8 +24,8 @@ class FeedbackController < ApplicationController
   end
 
   private
-    def feedback_params
-      params.require(:feedback).permit(:feedback).merge(application: session[:app]).merge(username: session[:username])
-    end
 
+  def feedback_params
+    params.require(:feedback).permit(:feedback).merge(application: session[:app]).merge(username: session[:username])
+  end
 end
