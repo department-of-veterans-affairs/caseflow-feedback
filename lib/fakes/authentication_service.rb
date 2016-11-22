0f5ddf8a4ea51205008d4bad3c641c5b0e6cd3a7
Alex Prokop <alex.prokop@gmail.com>
2016-11-22 15:05:47 -0500

    remove extraneous fake vacols auth

--- lib/fakes/authentication_service.rb
+++ lib/fakes/authentication_service.rb
@@ -5,17 +5,4 @@ class Fakes::AuthenticationService
   def self.default_user_session
     user_session
   end
-
-  def self.authenticate_vacols(regional_office, password)
-    normalized_ro = find_ro(regional_office)
-    actual_password = vacols_regional_offices[normalized_ro]
-    actual_password == password
-  end
-
-  def self.find_ro(regional_office)
-    # case-insensitive compare on all the keys
-    vacols_regional_offices.keys.find do |known_ro|
-      known_ro.casecmp(regional_office) == 0
-    end
-  end
 end
