de8c62a5dc5dbb4bd370ae9863227a6b11af7b74
Alex Prokop <alex.prokop@gmail.com>
2016-11-22 08:31:10 -0500

    configures session

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,10 @@
 # Be sure to restart your server when you modify this file.
+options = {
+  key: '_caseflow_session',
+  secure: Rails.env.production?,
+  expire_after: 24.hours
+}
 
-Rails.application.config.session_store :cookie_store, key: '_caseflow-feedback_session'
+options[:domain] = ENV["COOKIE_DOMAIN"] if ENV["COOKIE_DOMAIN"]
+
+Rails.application.config.session_store :cookie_store, options
