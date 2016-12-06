4a7ae85dae676392699c526ec327d277a536864b
Alex Prokop <alex.prokop@gmail.com>
2016-12-06 13:39:30 -0500

    adds tests

--- app/services/slack_service.rb
+++ app/services/slack_service.rb
@@ -2,13 +2,21 @@ require "json"
 require "httparty"
 
 class SlackService
+  def webhook_url
+    ENV["SLACK_WEBHOOK_URL"]
+  end
+
+  def http_service
+    HTTParty
+  end
+
   def send_new_feedback_notification(current_domain, subject)
-    return unless ENV["SLACK_WEBHOOK_URL"]
+    return unless webhook_url
 
     message = "New feedback was submitted about #{subject}. <#{current_domain}|Click here> to view it."
-    body = {"text": message}.to_json
-    params = {:body => body, :headers => { "Content-Type" => "application/json" } }
-    response = HTTParty.post(ENV["SLACK_WEBHOOK_URL"], params)
-    # TODO log slack feedback errors in Sentry
+    body = { "text": message }.to_json
+    params = { body: body, headers: { "Content-Type" => "application/json" } }
+    http_service.post(ENV["SLACK_WEBHOOK_URL"], params)
+    # TODO: log slack webhook errors in Sentry
   end
 end
