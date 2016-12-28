56c7eca3accabfba6d93112d5c52063d5263cd9c
Nemo Djuric <nemo.djuric@gmail.com>
2016-12-28 12:07:07 -0500

    Added jquery that checks on input fields and required styling. Modified Enter Feedback page.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,51 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+$(document).ready(function () {
+  $('.new_feedback').submit(function () {
+    window.validated = true;
+    var testEmail = /^[A-Z0-9._%+-]+@([A-Z0-9-]+\.)+[A-Z]{2,4}$/i;
+    var errors = {
+      "1" :   { "message": "Make sure you’ve entered a valid email address below." },
+      "2" :   { "message": "Make sure you’ve filled out the comment box below." }
+    };
+    // Check if textarea or input is empty
+    $($(this).find( 'textarea, input[type!="hidden"]' ).get().reverse()).each(function (index) {
+        // Add error class, add error message, change border css, add focus and adjust width
+        function addError(element) {
+          $(element).parent().addClass('usa-input-error');
+          $('.usa-input-error-message:eq('+(2-index) +')').text(errors[index]["message"]);
+          $(element).addClass('error-border');
+          $(element).css("width", 375);
+          $(element).focus();
+          validated = false;
+        };
+
+        function removeError(element) {
+          $(element).parent().removeClass();
+          $('.usa-input-error-message:eq('+(2-index)+')').empty();
+          $(element).removeClass('error-border');
+        }
+        // If it is empty add error
+        if ( ! $(this).val() ) {
+          addError($(this));
+        } else
+        {
+          // If there was an error, remove it since field is not empty
+          if ($(this).parent().attr('class') == 'usa-input-error') {
+            removeError($(this));
+          }
+          // Although field is not empty check if email is valid
+          if ((index == 1) && (!testEmail.test($(this).val()))){
+            addError($(this));
+          }
+        }
+
+    });
+
+    return validated
+  });
+
+
+
+});
