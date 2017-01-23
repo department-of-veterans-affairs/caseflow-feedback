319a626e359106ce46aa6f6686a2ac9bd770696e
aroltsch <anya.roltsch@gmail.com>
2017-01-23 12:01:28 -0500

    Add github issue to slack notification
    Create Github issue automatically when feedback is submitted

--- app/services/slack_service.rb
+++ app/services/slack_service.rb
@@ -2,6 +2,10 @@ require "json"
 require "httparty"
 
 class SlackService
+  include ActiveModel::Model
+
+  attr_accessor :current_domain, :subject, :github_url
+
   def webhook_url
     ENV["SLACK_WEBHOOK_URL"]
   end
@@ -10,15 +14,20 @@ class SlackService
     HTTParty
   end
 
-  def send_new_feedback_notification(current_domain, subject)
+  def send_new_feedback_notification
     return unless webhook_url
 
-    admin_route = current_domain + "admin"
-
-    message = "New feedback was submitted about #{subject}. <#{admin_route}|Click here> to view it."
     body = { "text": message }.to_json
     params = { body: body, headers: { "Content-Type" => "application/json" } }
     http_service.post(ENV["SLACK_WEBHOOK_URL"], params)
     # TODO: log slack webhook errors in Sentry
+  end
+
+  def message
+    admin_route = current_domain + "/admin"
+    msg = "New feedback was submitted about #{subject}. View it in "
+    msg += "<#{github_url}|Github> or " if github_url
+    msg += "<#{admin_route}|Admin Dashboard>."
+    msg
   end
 end
