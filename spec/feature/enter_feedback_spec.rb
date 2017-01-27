c1eb1e6949bdb179ea3c718ee2d24b50d9683567
Nemo Djuric <nemo.djuric@gmail.com>
2017-01-27 14:09:04 -0500

    Added tests for feedback, pii and email for db record.

--- spec/feature/enter_feedback_spec.rb
+++ spec/feature/enter_feedback_spec.rb
@@ -14,17 +14,24 @@ RSpec.feature "Enter feedback" do
     expect(page).to have_content("Contact email")
     expect(page).to have_css("#feedback_feedback")
     page.should have_link("Cancel")
-    fill_in "What's working well?", with: "Enter Feedback Spec"
-    fill_in "Contact email", with: "fk@va.gov"
+    fill_in "What's working well?", with: "Feedback"
     fill_in "If you are having an issue", with: "Veteran PII"
+    fill_in "Contact email", with: "email@va.gov"
     click_on "Send Feedback"
     expect(page).to have_content("Thanks for your feedback!")
     expect(page).to have_content("Back to Caseflow")
+    expect(Feedback.last.feedback).to eq("Feedback")
+    expect(Feedback.last.veteran_pii).to eq("Veteran PII")
+    expect(Feedback.last.contact_email).to eq("email@va.gov")
     click_on "Send in more feedback"
     expect(page).to have_content("Tell us about your experience with Caseflow")
-    fill_in "What's working well?", with: "Feedback"
+    fill_in "What's working well?", with: "Feedback Text"
+    fill_in "If you are having an issue", with: "Veteran PII Info"
     fill_in "Contact email", with: "fk@va.gov"
     click_on "Send Feedback"
+    expect(Feedback.last.feedback).to eq("Feedback Text")
+    expect(Feedback.last.veteran_pii).to eq("Veteran PII Info")
+    expect(Feedback.last.contact_email).to eq("fk@va.gov")
     expect(page).to have_content("Thanks for your feedback!")
     page.should have_link("Back to Caseflow", href: "caseflow.ds.va.gov")
   end
--- spec/models/feedback_spec.rb
+++ spec/models/feedback_spec.rb
@@ -2,10 +2,7 @@ require "rails_helper"
 
 RSpec.describe Feedback, type: :model do
   let(:feedback_no_email) { { subject: "eFolder Express", username: "alf", feedback: "gr8 app" } }
-  let(:feedback) do
-    { subject: "eFolder Express", username: "alf", feedback: "gr8 app",
-      contact_email: "fk@va.gov", veteran_pii: "PII" }
-  end
+  let(:feedback) { { subject: "eFolder Express", username: "alf", feedback: "gr8 app", contact_email: "fk@va.gov" } }
 
   after do
     Feedback.delete_all
@@ -59,12 +56,6 @@ RSpec.describe Feedback, type: :model do
   context "#issue_number" do
     it "returns issue number" do
       expect(Feedback.create(feedback).issue_number).to eq "95"
-    end
-  end
-
-  context "#veteran_pii" do
-    it "saves veteran_pii" do
-      expect(Feedback.create(feedback).veteran_pii).to eq "PII"
     end
   end
 end
