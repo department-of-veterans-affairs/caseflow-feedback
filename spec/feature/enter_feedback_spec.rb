898d0d2c76b5c16d89d73650e708a3ff5b1e6828
Nemo Djuric <nemo.djuric@gmail.com>
2017-01-31 13:58:31 -0500

    Added validation to the model and spec test for the feature.

--- spec/feature/enter_feedback_spec.rb
+++ spec/feature/enter_feedback_spec.rb
@@ -51,4 +51,62 @@ RSpec.feature "Enter feedback" do
     expect(page).not_to have_content("Make sure you’ve filled out the comment box below.")
     expect(page).not_to have_content("Make sure you’ve entered a valid email address below.")
   end
+
+  scenario "Test Characters remaining" do
+    visit "/feedback/new"
+    expect(page).not_to have_content("characters remaining")
+    fill_in "What's working well?", with: "Feedback"
+    expect(page).to have_content("1992 characters remaining")
+    fill_in "What's working well?", with:
+        "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget
+        dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur
+        ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla
+        consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.
+        In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede
+        mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean
+        vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac,
+        enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra
+        nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel
+        augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus,
+        tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque
+        sedipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec
+        odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis
+        ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit
+        amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue
+        velit cursus nunc, quis gravida magna mi a libero. Fusce vulputate eleifend sapien.
+        Vestibulum purus quam, scelerisque ut, mollis sed, nonummy id, metus. Nullam accumsan
+        lorem in dui. Cras ultricies mi eu turpis hendrerit fringilla. Vestibulum ante ipsum
+        primis in faucibus orci luctus et ultrices posuere cubilia Curae; In ac dui quis mi
+        consectetuer lacinia. Nam pretium turpis et arcu. Duis arcu tortor, suscipit eget,
+        imperdiet nec, imperdiet iaculis, ipsum. Sed aliquam ultrices mauris. Integer ante arcu,
+        accumsan a, consectetuer eget, posuere ut, mauris. Praesent adipiscing. Phasellus
+        ullamcorper ipsum rutrum nunc. Nunc nonummy metus. Vestib"
+    expect(page).to have_content("0 characters remaining")
+    fill_in "What's working well?", with:
+        "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget
+        dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur
+        ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla
+        consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.
+        In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede
+        mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean
+        vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac,
+        enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra
+        nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel
+        augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus,
+        tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque
+        sedipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec
+        odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis
+        ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit
+        amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue
+        velit cursus nunc, quis gravida magna mi a libero. Fusce vulputate eleifend sapien.
+        Vestibulum purus quam, scelerisque ut, mollis sed, nonummy id, metus. Nullam accumsan
+        lorem in dui. Cras ultricies mi eu turpis hendrerit fringilla. Vestibulum ante ipsum
+        primis in faucibus orci luctus et ultrices posuere cubilia Curae; In ac dui quis mi
+        consectetuer lacinia. Nam pretium turpis et arcu. Duis arcu tortor, suscipit eget,
+        imperdiet nec, imperdiet iaculis, ipsum. Sed aliquam ultrices mauris. Integer ante arcu,
+        accumsan a, consectetuer eget, posuere ut, mauris. Praesent adipiscing. Phasellus
+        ullamcorper ipsum rutrum nunc. Nunc nonummy metus. Vestib{"
+    expect(page).to have_content("0 characters remaining")
+    expect(page).not_to have_content("{")
+  end
 end
