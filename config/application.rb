88c1c80d7f51cd866f9c3fd0e36f0ef076b5b0e5
Nemo Djuric <nemo.djuric@gmail.com>
2018-02-21 01:25:46 -0500

    Put back config/application.rb setup

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,26 @@ module CaseflowFeedback
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
+    # Run "rake D time" for a list of tasks for finding time zone names. Default is UTC.
+    # config.time_zone = 'Central Time (US & Canada)'
+
+    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    # config.i18n.default_locale = :de
+
+    # Do not swallow errors in after_commit/after_rollback callbacks.
+    config.active_record.raise_in_transactional_callbacks = true
+
+    config.autoload_paths << Rails.root.join('lib')
+    config.autoload_paths << Rails.root.join('services')
+    config.autoload_paths += Dir[Rails.root.join('app', 'models', '{**}')]
+
+    # Gzip files when possible
+    config.middleware.use Rack::Deflater
+
+    # Default to no analytics (production only)
+    config.google_analytics_account = nil
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-resource :feedback
+	
+  resource :feedback
 
   root 'feedback#new'
 
@@ -7,5 +8,4 @@ resource :feedback
   get "health-check" => "health_checks#show"
   get "admin" => "feedback#admin"
   get 'logout', to: 'application#logout'
-
 end
