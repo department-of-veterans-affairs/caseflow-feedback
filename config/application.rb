74019fb9df97f5eead3689da97525d46caf7ed88
Nemo Djuric <nemo.djuric@gmail.com>
2018-02-22 12:51:42 -0500

    Add ApplicationRecord class

--- config/application.rb
+++ config/application.rb
@@ -20,9 +20,6 @@ module CaseflowFeedback
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
-    # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
-
     config.autoload_paths << Rails.root.join('lib')
     config.autoload_paths << Rails.root.join('services')
     config.autoload_paths += Dir[Rails.root.join('app', 'models', '{**}')]
