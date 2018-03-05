02f0038c8ff1517dc6bbf2a44613fec5b2af2588
Nemo Djuric <nemo.djuric@gmail.com>
2018-03-05 12:00:11 -0500

    Lint

--- app/controllers/health_checks_controller.rb
+++ app/controllers/health_checks_controller.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class HealthChecksController < ApplicationController
-
   def show
     render json: { healthy: true }.merge(Rails.application.config.build_version || {})
   end
