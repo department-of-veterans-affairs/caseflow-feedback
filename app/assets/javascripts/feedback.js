2d0c04bab43626618d8d3123e25df6c9664af2af
Nemo Djuric <ndjuric.va@gmail.com>
2018-06-27 16:49:04 -0400

    Change email pattern to allow apostrophe (#189)
    * Change email pattern to allow apostrophe
    
    * Update sprockets

--- app/assets/javascripts/feedback.js
+++ app/assets/javascripts/feedback.js
@@ -16,7 +16,7 @@ $(document).ready(function () {
     "incompleteError": incompleteErrorMessages,
     "patternError": patternErrorMessages
   };
-  var emailPattern = /^[A-Z0-9._%+-]+@([A-Z0-9-]+\.)+[A-Z]{2,4}$/i;
+  var emailPattern = /^['A-Z0-9._%+-]+@([A-Z0-9-]+\.)+[A-Z]{2,4}$/i;
 
   function init() {
     initState();
